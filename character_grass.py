83ef002e2676a34940824e5831bad47c797ab630
2022182024 <lhnyoon@tukorea.ac.kr>
2023-09-20 17:19:33 +0900

    사각형만

--- character_grass.py
+++ character_grass.py
@@ -5,12 +5,38 @@ open_canvas()
 grass = load_image('grass.png')
 character =load_image('character.png')
 
-x = 0
-while(x<800):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, 90)
-    x = x + 2
-    delay(0.01)
+while(True):
+    x = 0
+    y = 100
+    while(x<=790):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, 90)
+        x = x + 2
+        delay(0.01)
+
+    if(x>= 790):
+        while(y<=570):
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            character.draw_now(x, y)
+            y = y + 2
+            delay(0.01)
+
+    if(y >=570):
+        while(x>=0):
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            character.draw_now(x, y)
+            x = x - 2
+            delay(0.01)
+
+    if(x<=0):
+        while(y >= 90):
+            clear_canvas_now()
+            grass.draw_now(400, 30)
+            character.draw_now(x, y)
+            y = y - 2
+            delay(0.01)
 
 close_canvas()
